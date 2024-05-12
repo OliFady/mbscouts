926e7d7d6ea62ff78a567623d566f855667ac7f5
OliFady <oliver20191702008@cis.asu.edu.eg>
2024-05-12 19:17:47 +0300

    increased-puppeteer max timeout

--- scully.mbs.config.ts
+++ scully.mbs.config.ts
@@ -5,6 +5,9 @@ export const config: ScullyConfig = {
   projectName: 'mbs',
   distFolder: './dist/mbs', // output directory of your Angular build artifacts
   sourceRoot: "./src",
+  puppeteerLaunchOptions: {
+    timeout: 120000, // Increase timeout to 120 seconds (2 minutes)
+  },
   outDir: './dist/static', // directory for scully build artifacts
   defaultPostRenderers: [],
   routes: {},
